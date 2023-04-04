14db37d6038a15e1c3ab830d61d85a5a7c8a8e8c
Yun Jang <jang.yunj7@gmail.com>
2023-04-03 23:57:34 -0400

    initial commit

---
+++ MyCSAT/Data/MVCDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyCSAT.Models.Domain;
+
+namespace MyCSAT.Data
+{
+    public class MVCDbContext : DbContext
+    {
+        //constructor with options
+        public MVCDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        //properties
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ MyCSAT/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyCSAT.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
